e21123f94e1bd86c6f1f9f74239c6a93529a4271
mardiana36 <kelikimardiana@gmail.com>
2024-09-13 10:52:05 +0800

    website tracoid

---
+++ js/footer.js
@@ -0,0 +1,46 @@
+$(document.body).append(`
+    <footer>
+        <section class="container containerFooter">
+            <div class="content contentFooter">
+                <div class="cardFooter-1">
+                    <div>
+                        <h1 class="logo-web logoFooter">Tracoid</h1>
+                        <h2>Tracoid Space</h2>
+                        <p>Jl. Batik Kulintang No.50, Sukanila, Kec. Tegallalang , Kota Bandung Jawa Barat 436363</p>
+                        <h2>Ikuti Kami</h2>
+                        <div class="socmed">
+                            <i class='bx bxl-tiktok'></i>
+                            <i class='bx bxl-instagram-alt'></i>
+                            <i class='bx bxl-facebook-square'></i>
+                            <i class='bx bxl-youtube'></i>
+                            <i class='bx bxl-twitter'></i>
+                        </div>
+                    </div>
+                    <div>
+                        <h3>Mengasah Potensi, Menciptakan Ahli.</h3>
+                        <a href="">Tentang Kami</a>
+                    </div>
+                    <div class="contactFooter">
+                        <h2>Kontak</h2>
+                        <a href="">Hubungi Kami</a>
+                        <a href="">FAQ</a>
+                    </div>
+                    <div class="downloadApp">
+                        <h2>Dapatkan Aplikasi Tracoid</h2>
+                        <a href="" class="googlePlay">
+                            <i class='bx bxl-play-store' ></i>
+                            <p>Play Store</p>
+                        </a>
+                        <a href="" class="appStore">
+                            <i class='bx bxl-apple' ></i>
+                            <p>App Store</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="cardFooter-2">
+            <p><i class='bx bx-copyright'></i> 2024 Tracoid. All right reserved. Images designed by <a href="https://www.freepik.com" target="_blank"> Freepik.</a></p>
+        </div>
+    </footer>
+    `)
---
+++ js/initAos.js
@@ -0,0 +1,5 @@
+$(document.body).append(`
+    <script src="../lib/Animate On Scroll v2.3.2/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>`);
