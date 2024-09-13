b1bbe4b17b4e9184f7fb7236429987385720cffe
mardiana36 <135509698+mardiana36@users.noreply.github.com>
2024-09-13 10:06:23 +0800

    update initAos.js

--- js/initAos.js
+++ js/initAos.js
@@ -1,5 +1,5 @@
 $(document.body).append(`
-    <script src="/lib/Animate On Scroll v2.3.2/dist/aos.js"></script>
+    <script src="../lib/Animate On Scroll v2.3.2/dist/aos.js"></script>
     <script>
         AOS.init();
     </script>`);
