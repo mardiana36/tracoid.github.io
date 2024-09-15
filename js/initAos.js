23cf786b84fcbf7d0941a8ce0a614cc439fd265c
mardiana36 <kelikimardiana@gmail.com>
2024-09-15 11:05:08 +0800

    update

--- js/initAos.js
+++ js/initAos.js
@@ -1,5 +1,15 @@
 $(document.body).append(`
     <script src="lib/Animate On Scroll v2.3.2/dist/aos.js"></script>
     <script>
-        AOS.init();
-    </script>`);
+        const checkWidth = () => {
+            AOS.init({
+                disable: function() {
+                    return window.innerWidth <= 1136;
+                }
+            });
+        };
+
+        $(window).resize(checkWidth);
+        checkWidth();
+    </script>
+`);
